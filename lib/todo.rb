09ee1f30340f2d798ffd23a48378e0d2b22f98af
htmmc <hitomi.mzgk@gmail.com>
2015-03-24 15:46:53 -0400

    re-did some of the file clean up

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,6 +1,4 @@
 require 'todo/item'
 module Todo
-	def self.answer
-		42
-	end 
+
 end 
--- test/todo_test.rb
+++
@@ -1,7 +0,0 @@
-require './config/environment'
-
-class TodoTest < MiniTest::Unit::TestCase
-  def test_it_works
-  	assert_equal 42, Todo.answer
-  end
-end 
